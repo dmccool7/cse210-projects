65311eade982814237f7485876f1c56dda2d5b44
dmccool7 <dmccool@byupathway.edu>
2025-05-18 21:06:03 -0500

    journal program

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,18 +1,62 @@
+// Exceed
 using System;
 using System.Diagnostics;
+using System.Xml.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to the Journal Program!");
-        Console.WriteLine("Please select one of the following choices:");
-        Console.WriteLine("1. Write");
-        Console.WriteLine("2. Display");
-        Console.WriteLine("3. Load");
-        Console.WriteLine("4. Save");
-        Console.WriteLine("5. Quit");
-        Console.Write("What would you like to do? ");
-        string choice = Console.ReadLine();   
+        Journal theJournal = new Journal();
+        Prompt promptGenerator = new Prompt();
+
+        string choice = "";
+        while (choice != "5")
+        {
+            Console.WriteLine("Welcome to the Journal Program!");
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+                Console.WriteLine(prompt);
+                string entryResponse = Console.ReadLine();
+
+                Entry entry = new Entry
+                {
+                    _date = DateTime.Now.ToString("MM-dd-yyyy"),
+                    _promptText = prompt,
+                    _entryText = entryResponse
+                };
+
+                theJournal.AddEntry(entry);
+            }
+
+            else if (choice == "2")
+            {
+                theJournal.DisplayAll();
+            }
+
+            else if (choice == "3")
+            {
+                Console.Write("What is the filename? ");
+                string file = Console.ReadLine();
+                theJournal.LoadFromFile(file);
+            }
+
+            else if (choice == "4")
+            {
+                Console.Write("What is the filename? ");
+                string file = Console.ReadLine();
+                theJournal.SaveToFile(file);
+            }
+        }
     }
 }
--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -1,9 +1,23 @@
 public class Prompt
 {
-    public List<string> _prompts;
+    public List<string> _prompts = new List<string>
+    {
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?",
+        "What is something I learned today?",
+        "Who did something kind to me today?",
+        "What act of kindness did I do today?",
+        "How did I improve myself today?",
+        "How did I strengthen a relationship today?"
+    };
 
     public string GetRandomPrompt()
     {
-        return "";
+        Random prompt = new Random();
+        int indexPrompt = prompt.Next(_prompts.Count);
+        return _prompts[indexPrompt];
     }
 }

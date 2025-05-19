bc86ef2fc952e8d116555d13a481c754a21fe725
dmccool7 <dmccool@byupathway.edu>
2025-05-18 21:20:06 -0500

    excced requirements added

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,5 +1,9 @@
-// Exceed
+// I added a password needed to access the journal program because I think a reason that people don't journal
+// is because they have a fear of someone else reading it. This way, they do not have to worry unless they
+// pass down the password to someone else at some point.
+
 using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Xml.Serialization;
 
@@ -10,53 +14,67 @@ class Program
         Journal theJournal = new Journal();
         Prompt promptGenerator = new Prompt();
 
-        string choice = "";
-        while (choice != "5")
+        string passphrase = "dallasjournal7";
+
+        Console.Write("Enter Password to Access Journal: ");
+        string passAttempt = Console.ReadLine();
+
+        if (passAttempt == passphrase)
         {
-            Console.WriteLine("Welcome to the Journal Program!");
-            Console.WriteLine("Please select one of the following choices:");
-            Console.WriteLine("1. Write");
-            Console.WriteLine("2. Display");
-            Console.WriteLine("3. Load");
-            Console.WriteLine("4. Save");
-            Console.WriteLine("5. Quit");
-            Console.Write("What would you like to do? ");
-            choice = Console.ReadLine();
 
-            if (choice == "1")
+            string choice = "";
+            while (choice != "5")
             {
-                string prompt = promptGenerator.GetRandomPrompt();
-                Console.WriteLine(prompt);
-                string entryResponse = Console.ReadLine();
+                Console.WriteLine("Welcome to the Journal Program!");
+                Console.WriteLine("Please select one of the following choices:");
+                Console.WriteLine("1. Write");
+                Console.WriteLine("2. Display");
+                Console.WriteLine("3. Load");
+                Console.WriteLine("4. Save");
+                Console.WriteLine("5. Quit");
+                Console.Write("What would you like to do? ");
+                choice = Console.ReadLine();
 
-                Entry entry = new Entry
+                if (choice == "1")
                 {
-                    _date = DateTime.Now.ToString("MM-dd-yyyy"),
-                    _promptText = prompt,
-                    _entryText = entryResponse
-                };
+                    string prompt = promptGenerator.GetRandomPrompt();
+                    Console.WriteLine(prompt);
+                    string entryResponse = Console.ReadLine();
 
-                theJournal.AddEntry(entry);
-            }
+                    Entry entry = new Entry
+                    {
+                        _date = DateTime.Now.ToString("MM-dd-yyyy"),
+                        _promptText = prompt,
+                        _entryText = entryResponse
+                    };
 
-            else if (choice == "2")
-            {
-                theJournal.DisplayAll();
-            }
+                    theJournal.AddEntry(entry);
+                }
 
-            else if (choice == "3")
-            {
-                Console.Write("What is the filename? ");
-                string file = Console.ReadLine();
-                theJournal.LoadFromFile(file);
-            }
+                else if (choice == "2")
+                {
+                    theJournal.DisplayAll();
+                }
 
-            else if (choice == "4")
-            {
-                Console.Write("What is the filename? ");
-                string file = Console.ReadLine();
-                theJournal.SaveToFile(file);
+                else if (choice == "3")
+                {
+                    Console.Write("What is the filename? ");
+                    string file = Console.ReadLine();
+                    theJournal.LoadFromFile(file);
+                }
+
+                else if (choice == "4")
+                {
+                    Console.Write("What is the filename? ");
+                    string file = Console.ReadLine();
+                    theJournal.SaveToFile(file);
+                }
             }
+        }
+
+        else
+        {
+            Console.WriteLine("Wrong.");
         }
     }
 }

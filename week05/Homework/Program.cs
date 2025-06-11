e50f6a259de63b268d5150ceedb0dfca6bba79ec
dmccool7 <dallasmccool@outlook.com>
2025-06-11 11:24:13 -0500

    homework practice

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment assignment1 = new MathAssignment("Dallas McCool", "Statistics", "7.3", "11-24");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine(assignment1.GetHomeworkList());
+
+        WritingAssignment assignment2 = new WritingAssignment("Mary McCool", "Creative Writing", "Next Summer");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetWritingInformation());
     }
 }

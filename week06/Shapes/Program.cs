6b2bc17afbdb55ee8aa7b6d8e2faacd6c38f1db0
dmccool7 <dmccool@byupathway.edu>
2025-06-16 18:37:20 -0500

    shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Red", 8);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Yellow", 7, 5);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Green", 3);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area:0.00}.");
+        }
     }
 }
